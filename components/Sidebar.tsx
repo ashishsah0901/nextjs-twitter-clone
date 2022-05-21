fcebd250ed4b0b551633677e7dc361ee52060458
ashishsah0901 <69725687+ashishsah0901@users.noreply.github.com>
2022-05-22 02:36:51 +0530

    Sidebar and Widget Completed

---
+++ components/Sidebar.tsx
@@ -0,0 +1,37 @@
+import {
+  BellIcon,
+  HashtagIcon,
+  BookmarkIcon,
+  CollectionIcon,
+  DotsCircleHorizontalIcon,
+  MailIcon,
+  UserIcon,
+  HomeIcon,
+} from '@heroicons/react/outline'
+import SidebarRow from './SidebarRow'
+import Image from 'next/image'
+
+const Sidebar = () => {
+  return (
+    <div className="col-span-2 flex flex-col items-center px-4 md:items-start">
+      <div className="m-3 h-10 w-10">
+        <Image
+          src="https://links.papareact.com/drq"
+          alt=""
+          height={100}
+          width={100}
+        />
+      </div>
+      <SidebarRow Icon={HomeIcon} title="Home" />
+      <SidebarRow Icon={HashtagIcon} title="Explorer" />
+      <SidebarRow Icon={BellIcon} title="Notifications" />
+      <SidebarRow Icon={MailIcon} title="Messages" />
+      <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
+      <SidebarRow Icon={CollectionIcon} title="Lists" />
+      <SidebarRow Icon={UserIcon} title="Sign In" />
+      <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
+    </div>
+  )
+}
+
+export default Sidebar
